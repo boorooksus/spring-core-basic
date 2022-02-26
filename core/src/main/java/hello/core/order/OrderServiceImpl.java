ea65a6fa1df3f23cb8551c3fa1248a165dde21c1
boorooksus <boorooksus@gmail.com>
2022-02-26 11:57:01 +0900

    의존관계 자동 주입

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,28 +1,47 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // setter 주입 이용하려면 final 지워야함
     private final MemberRepository memberRepository;
+//    private MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        System.out.println("2. memberRepository: " + memberRepository);
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        System.out.println("2. discountPolicy: " + discountPolicy);
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 롬복으로 생성자 생략 가능
     // 생성자가 1개일 땐, '@Autowired' 생략 가능
 //    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository: " + memberRepository);
-        System.out.println("discountPolicy: " + discountPolicy);
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+//        System.out.println("1. memberRepository: " + memberRepository);
+//        System.out.println("1. discountPolicy: " + discountPolicy);
 
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
